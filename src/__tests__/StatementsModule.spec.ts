b931a3c7f72467333844ac914b55d256a2dd5883
Lucas Levandoski <lucas-levandoski@hotmail.com>
2022-07-19 21:08:04 -0300

    GetStatemenetUseCase unity tests

--- src/__tests__/StatementsModule.spec.ts
+++ src/__tests__/StatementsModule.spec.ts
@@ -1,8 +1,11 @@
+import { emit } from "process";
 import { InMemoryStatementsRepository } from "../modules/statements/repositories/in-memory/InMemoryStatementsRepository"
 import { CreateStatementError } from "../modules/statements/useCases/createStatement/CreateStatementError";
 import { CreateStatementUseCase } from "../modules/statements/useCases/createStatement/CreateStatementUseCase";
 import { GetBalanceError } from "../modules/statements/useCases/getBalance/GetBalanceError";
 import { GetBalanceUseCase } from "../modules/statements/useCases/getBalance/GetBalanceUseCase";
+import { GetStatementOperationError } from "../modules/statements/useCases/getStatementOperation/GetStatementOperationError";
+import { GetStatementOperationUseCase } from "../modules/statements/useCases/getStatementOperation/GetStatementOperationUseCase";
 import { InMemoryUsersRepository } from "../modules/users/repositories/in-memory/InMemoryUsersRepository";
 
 
@@ -10,6 +13,7 @@ const statementsRepository = new InMemoryStatementsRepository();
 const usersRepository = new InMemoryUsersRepository();
 const createStatementUseCase = new CreateStatementUseCase(usersRepository, statementsRepository);
 const getBalanceUseCase = new GetBalanceUseCase(statementsRepository, usersRepository);
+const getStatementOperationUseCase = new GetStatementOperationUseCase(usersRepository, statementsRepository);
 
 enum OperationType {
   DEPOSIT = 'deposit',
@@ -59,5 +63,35 @@ describe('GetBalanceUseCase', () => {
     expect(async () => {
       await getBalanceUseCase.execute({ user_id: '11' });
     }).rejects.toBeInstanceOf(GetBalanceError);
+  });
+})
+
+describe('GetStatementOperationUseCase', () => {
+  it('should be able to return the operation', async () => {
+    const user = await usersRepository.create({ name: 'test', email: 'test@test', password: '1234' });
+
+    const statement = await statementsRepository.create({ amount: 300, description: '', type: OperationType.DEPOSIT, user_id: user.id! })
+
+    const operation = await getStatementOperationUseCase.execute({ user_id: user.id!, statement_id: statement.id! });
+
+    expect(operation).toEqual(statement);
+  });
+
+  it('should fail for user not found', async () => {
+    const user = await usersRepository.create({ name: 'test', email: 'test@test', password: '1234' });
+    const statement = await statementsRepository.create({ amount: 300, description: '', type: OperationType.DEPOSIT, user_id: user.id! })
+
+    expect(async () => {
+      await getStatementOperationUseCase.execute({ user_id: '11', statement_id: statement.id! });
+    }).rejects.toBeInstanceOf(GetStatementOperationError.UserNotFound);
+  });
+
+  it('should fail for statement not found', async () => {
+    const user = await usersRepository.create({ name: 'test', email: 'test@test', password: '1234' });
+    const statement = await statementsRepository.create({ amount: 300, description: '', type: OperationType.DEPOSIT, user_id: user.id! })
+
+    expect(async () => {
+      await getStatementOperationUseCase.execute({ user_id: user.id!, statement_id: '11' });
+    }).rejects.toBeInstanceOf(GetStatementOperationError.StatementNotFound);
   });
 })
