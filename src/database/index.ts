87c5bfc762311a590e636df3e25c55ee30da8200
Lucas Levandoski <lucas-levandoski@hotmail.com>
2022-07-20 23:30:52 -0300

    new integration test, not working yet

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,16 @@
-import { createConnection } from 'typeorm';
+import { env } from 'process';
+import { createConnection, getConnectionOptions } from 'typeorm';
 
-(async () => await createConnection())();
+(
+  async () => {
+    const defaultOptions = await getConnectionOptions();
+
+    return await createConnection(
+      Object.assign(defaultOptions, {
+        database: env.NODE_ENV === 'INTEGRATION_TESTS'
+          ? 'integration_tests'
+          : defaultOptions.database
+      })
+    )
+  }
+)();
