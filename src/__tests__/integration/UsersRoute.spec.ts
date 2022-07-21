1aafdcb0495f8b184b60c0845b4fd20da2b076e0
Lucas Levandoski <lucas-levandoski@hotmail.com>
2022-07-21 00:24:09 -0300

    full user test working for 200 and 201 results

--- src/__tests__/integration/UsersRoute.spec.ts
+++ src/__tests__/integration/UsersRoute.spec.ts
@@ -17,13 +17,33 @@ describe('Create User Route', () => {
     await connection.close();
   })
 
-  it('should be able to create a user', async () => {
-    const response = await request(app).post('/api/v1/users').send({
-      name: 'int-test',
-      email: 'int@test',
+  it('should be able to create, authenticate a user and get his profile ', async () => {
+    const user = {
+      name: 'create-user',
+      email: 'create@user',
       password: '1234'
+    }
+
+    const createUserRes = await request(app).post('/api/v1/users').send({
+      name: user.name,
+      email: user.email,
+      password: user.password
     });
 
-    expect(response.status).toEqual(201);
+    expect(createUserRes.status).toBe(201);
+
+    const authUserRes = await request(app).post('/api/v1/sessions').send({
+      email: user.email,
+      password: user.password
+    })
+
+    expect(authUserRes.status).toBe(200);
+    expect(authUserRes.body.token).toBeTruthy();
+
+    const userProfileRes = await request(app).get('/api/v1/profile')
+      .set('Authorization', 'bearer ' + authUserRes.body.token);
+
+    expect(userProfileRes.status).toBe(200);
+    expect(userProfileRes.body.name).toBe(user.name);
   })
 })
